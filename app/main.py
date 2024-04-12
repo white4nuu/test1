c720f3102434cca7b4417f5187aed83b03040b99
white4nuu <beak3844@gmail.com>
2024-04-12 16:47:11 +0900

    test
    test

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+IT = None
+
 class Item(BaseModel):
     name: str
     price: float
@@ -15,12 +17,23 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
+if IT is None:
+    @app.post("/items/{item_id}")
+    def write_item(item_id: str, item: Item):
+        IT = Item
+        return {"item_name": item.name, "item_id": item_id}
+    
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.post("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+#@app.post("/items/{item_id}")
+#def update_item(item_id: int, item: Item):
+#   return {"item_name": item.name, "item_id": item_id}
