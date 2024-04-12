36c6f5f9cd26b886c704378f805bbf3b50013b2f
white4nuu <beak3844@gmail.com>
2024-04-12 16:18:26 +0900

    test
    test

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
